0f22b520de456813fff67de7ed63e6d0cb5e5e2b
ubaidh <38114742+ubaidh@users.noreply.github.com>
2020-10-09 23:27:36 +0530

    Update Crop_Resize_Images.py

--- Basics/Crop_Resize_Images.py
+++ Basics/Crop_Resize_Images.py
@@ -11,8 +11,9 @@ print(imgResize.shape)
 imgCropped = img[300:540,430:480]
 imCropResize  = cv2.resize(imgCropped,(img.shape[1],img.shape[0]))
 
+
 cv2.imshow("Road",img)
 cv2.imshow("Road Resized",imgResize)
 cv2.imshow("Road Cropped",imgCropped)
 cv2.imshow("Road Cropped Resized",imCropResize)
-cv2.waitKey(0)
+cv2.waitKey(1)
